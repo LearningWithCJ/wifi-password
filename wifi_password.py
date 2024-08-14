af1e227ce2cce38b1b8475bb141062a6c22c7777
CJ <110461799+LearningWithCJ@users.noreply.github.com>
2024-08-14 23:42:57 +0330

    Add files via upload

---
+++ wifi_password.py
@@ -0,0 +1,32 @@
+#
+#  _                                _                __          __ _  _    _        _____      _ 
+# | |                              (_)               \ \        / /(_)| |  | |      / ____|    | |
+# | |      ___   __ _  _ __  _ __   _  _ __    __ _   \ \  /\  / /  _ | |_ | |__   | |         | |
+# | |     / _ \ / _` || '__|| '_ \ | || '_ \  / _` |   \ \/  \/ /  | || __|| '_ \  | |     _   | |
+# | |____|  __/| (_| || |   | | | || || | | || (_| |    \  /\  /   | || |_ | | | | | |____| |__| |
+# |______|\___| \__,_||_|   |_| |_||_||_| |_| \__, |     \/  \/    |_| \__||_| |_|  \_____|\____/ 
+#                                              __/ |                                              
+#                                             |___/                         -  By CJ
+#
+# YouTube : www.youtube.com/@LearningWithCJ
+# GitHub  : www.github.com/Carl-Johnson1976
+# Telegram: t.me/LearningWithCJ
+#
+
+
+import subprocess
+
+
+
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode().split('\n')
+profiles = [i.split(":")[1][1:-1] for i in data if 'All User Profile' in i]
+
+information = []
+for i in profiles:
+    results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('latin-1').split('\n')
+    results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+    finall = "{:30} | {}\n".format(i, results)
+    information.append(finall)
+res = "".join(information)
+
+print(res)
